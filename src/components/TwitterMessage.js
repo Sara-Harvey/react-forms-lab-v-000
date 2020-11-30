33300ce0e1e50ff29d33ef6bd17d5787f44fc983
Sara Harvey <saraharveynyc@gmail.com>
2020-11-30 13:32:05 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -11,7 +11,7 @@ class TwitterMessage extends React.Component {
 
   handleChange = event => {
     this.setState({
-      value: event.target.value
+      [event value: event.target.value
     })
   }
 
