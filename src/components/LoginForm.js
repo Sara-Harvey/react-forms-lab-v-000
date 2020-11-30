cbb92bcaf29910a000f2d7656fc34f5437f024cf
Sara Harvey <saraharveynyc@gmail.com>
2020-11-30 13:09:49 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -29,7 +29,7 @@ class LoginForm extends React.Component {
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.username} onChange={this.handleLogin}/>
           </label>
         </div>
         <div>
