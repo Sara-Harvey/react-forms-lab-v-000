e8b9e5ed1a156af202990d69800f07c3992eb94e
Sara Harvey <saraharveynyc@gmail.com>
2020-11-30 13:23:58 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -18,6 +18,7 @@ class LoginForm extends React.Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
+    
     if (this.state.username && this.state.password) {
       console.log(`Welcome ${this.state.username}`);
     }
